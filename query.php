7b4d3682030d558ecc9e2d7b4799ad8bb7dffda4
cterndrup <cterndrup@cs.ucla.edu>
2015-10-18 18:55:46 -0700

    Added list of relations and their schema

--- query.php
+++ query.php
@@ -6,6 +6,19 @@
 <!-- instructions for using query interface -->
 <p>Enter a MySQL query in the text area below to query the Movie database!</p>
 <p>Example: SELECT title FROM Movie WHERE rating='PG-13';</p>
+<p>Available Relations</p>
+<ul>
+<li>Movie</li>
+<li>Actor</li>
+<li>Director</li>
+<li>MovieGenre</li>
+<li>MovieDirector</li>
+<li>MovieActor</li>
+<li>Review</li>
+<li>MaxPersonID</li>
+<li>MaxMovieID</li>
+</ul>
+<h2>Add Schema above to improve instructions</h2>
 
 <form method="GET" action="query.php">
 <!-- TEXTAREA for entering query -->

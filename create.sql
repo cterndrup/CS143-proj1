72c67eb4586150368e989ede5c6b6e58632c4e3d
cterndrup <cterndrup@cs.ucla.edu>
2015-10-19 00:34:49 -0700

    add check constraints to tables

--- create.sql
+++ create.sql
@@ -1,35 +1,44 @@
 -- Script to create tables for movie database
 
 -- Movie table
--- constraints: id is a primary key, and thus no two tuples 
+-- constraints: 
+-- (1) id is a primary key, and thus no two tuples 
 -- in the Movie table may have the same id
+-- (2) the check constraint ensures every movie must have a title
 CREATE TABLE Movie (
     id INTEGER PRIMARY KEY,
     title VARCHAR(100),
     year INTEGER,
     rating VARCHAR(10),
-    company VARCHAR(50)) ENGINE=INNODB;
+    company VARCHAR(50),
+    CHECK(title IS NOT NULL)) ENGINE=INNODB;
 
 -- Actor table
--- constraints: id is a primary key, and thus no two tuples
+-- constraints: 
+-- (1) id is a primary key, and thus no two tuples
 -- in the Actor table may have the same id
+-- (2) the check constraint ensures each actor must have a dob
 CREATE TABLE Actor (
     id INTEGER PRIMARY KEY,
     last VARCHAR(20),
     first VARCHAR(20),
     sex VARCHAR(6),
     dob DATE,
-    dod DATE) ENGINE=INNODB;
+    dod DATE,
+    CHECK(dob IS NOT NULL)) ENGINE=INNODB;
 
 -- Director table
--- constraints: id is a primary key, and thus no two tuples
+-- constraints: 
+-- (1) id is a primary key, and thus no two tuples
 -- in the Director table may have the same id
+-- (2) the check constraint ensures each director must have a dob
 CREATE TABLE Director(
     id INTEGER PRIMARY KEY,
     last VARCHAR(20),
     first VARCHAR(20),
     dob DATE,
-    dod DATE) ENGINE=INNODB;
+    dod DATE,
+    CHECK(dob IS NOT NULL)) ENGINE=INNODB;
 
 -- MovieGenre table
 -- constraints: mid references Movie.id, and thus each mid
@@ -59,22 +68,27 @@ CREATE TABLE MovieDirector(
 -- (2) aid references Actor.id, and thus each aid
 -- value in the MovieActor table must reference and id value
 -- in the Actor table
+-- (3) the check constraint ensures every actor must have a role
+-- in the movie(s) he or she appears in
 CREATE TABLE MovieActor(
     mid INTEGER REFERENCES Movie(id),
     aid INTEGER REFERENCES Actor(id),
-    role VARCHAR(50)) ENGINE=INNODB;
+    role VARCHAR(50),
+    CHECK(role IS NOT NULL)) ENGINE=INNODB;
 
 -- Review table
 -- constraints:
--- mid references Movie.id, and thus each mid
+-- (1) mid references Movie.id, and thus each mid
 -- value in the Review table must reference an id value
 -- in the Movie table
+-- (2) the check constraint ensures each review should have a name
 CREATE TABLE Review(
     name VARCHAR(20),
     time TIMESTAMP,
     mid INTEGER REFERENCES Movie(id),
     rating INTEGER,
-    comment VARCHAR(500)) ENGINE=INNODB;
+    comment VARCHAR(500),
+    CHECK(name IS NOT NULL)) ENGINE=INNODB;
 
 -- MaxPersonID table
 CREATE TABLE MaxPersonID(id INTEGER) ENGINE=INNODB;

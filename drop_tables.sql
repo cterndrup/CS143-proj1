855a348b8b8fa1f3861da633237101bbfff19e97
cterndrup <cterndrup@cs.ucla.edu>
2015-10-20 02:09:53 -0700

    add script to drop all tables

---
+++ drop_tables.sql
@@ -0,0 +1,10 @@
+-- drop all tables in db
+DROP TABLE MaxPersonID;
+DROP TABLE MaxMovieID;
+DROP TABLE MovieGenre;
+DROP TABLE MovieDirector;
+DROP TABLE MovieActor;
+DROP TABLE Review;
+DROP TABLE Movie;
+DROP TABLE Actor;
+DROP TABLE Director;

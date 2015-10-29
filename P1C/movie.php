3970276d52238f2a3db67c7aa6b67acbc84ba037
cterndrup <cterndrup@cs.ucla.edu>
2015-10-29 01:48:29 -0700

    add interface to post review

--- P1C/movie.php
+++ P1C/movie.php
@@ -34,8 +34,8 @@
     }
 
     // create query to get all movie info
-    $title = "'".$title."'";
-    $query = "select * from Movie where title=".$title;
+    $qtitle = "'$title'";
+    $query = "select * from Movie where title=".$qtitle;
     
     // issue query
     $resource = mysql_query($query, $db_connection);
@@ -55,7 +55,7 @@
     echo "<h3>Actors/Actresses</h3>"; 
     // query for all actors/actresses in movie
     $mid = $row["id"];
-    $query = "select Actor.first, Actor.last from Actor, MovieActor where MovieActor.mid=$mid and MovieActor.aid=Actor.id order by Actor.first asc";
+    $query = "select Actor.first, Actor.last from Actor, MovieActor where MovieActor.mid=$mid and MovieActor.aid=Actor.id order by Actor.last asc";
 
     // issue query
     $resource = mysql_query($query, $db_connection);
@@ -64,7 +64,10 @@
         exit("<br><strong>Error: ".$error."</strong>");
     } 
     // exit if no results returned from query
-    if (!mysql_num_rows($resource)) exit(0);
+    if (!mysql_num_rows($resource)) {
+        echo "N/A";
+        exit(0);
+    }
 
     // show all actors/actresses in movie
     while ($row = mysql_fetch_array($resource, MYSQL_ASSOC)) {
@@ -72,11 +75,21 @@
         $last = $row["last"];
         $full_name = "$first $last";
         $actorURL = "actor.php?first=$first&last=$last";
-        echo "<a href=$actorURL>$full_name</a><br>";
+        echo "<a href='$actorURL'>$full_name</a><br>";
     }
-    
+   
+    // Reviews section
+    $actionURL = "post_review.php";
     echo "<h3>Reviews</h3>"; 
-    echo "ADD REVIEW -- INSERT CAPABILITY HERE";
+    echo "<h4>Add a Review!</h4>";
+    echo "<form action='$actionURL' method='POST'>";
+    echo "Your name: <input type='text' name='name'><br>";
+    echo "Movie: <input type='text' name='movie' value='$title' readonly><br>";
+    echo "Rating (1 to 5): <input type='text' name='rating'><br>";
+    echo "Comments: <br><textarea rows='10' cols='50' name='comment'></textarea><br>";
+    echo "<input type='submit' value='Submit!'>";
+    echo "</form>";
+
     // query for all user comments and allow user to add comment
     $query = "select * from Review where mid=".$mid;
 
